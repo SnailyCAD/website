f0d8cc3adb7c590b098f54d377d9984d01b76368
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-01-24 11:44:51 +0100

    :hammer: add SEO

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -12,14 +12,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta name="robots" content="index, follow" />
   <title>SnailyCAD - The Free and open-source CAD/MDT for FiveM</title>
-  <meta
-    name="description"
-    content="SnailyCAD is a free and open-source CAD/MDT made to feel and look modern with all the wanted features to fit your roleplay needs."
-  />
-  <meta
-    name="keywords"
-    content="CAD, MDT, fivem, leo, open source, roleplay, law enforcement, fire department, ems, bleeter, free, customizable"
-  />
+
   <link rel="canonical" href="https://snailycad.caspertheghost.me" />
 </svelte:head>
 
