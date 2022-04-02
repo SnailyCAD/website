7a62cc9f6d25e6be752690c2cbb8473748e5b5c4
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-04-02 12:27:03 +0200

    :hammer: chore: bump deps

--- src/lib/HeroSection.svelte
+++ src/lib/HeroSection.svelte
@@ -5,17 +5,17 @@
 
 <section class="flex flex-col items-center justify-center w-full max-w-5xl py-20 mt-36 gap-y-6">
   <h1
-    class="max-w-5xl text-3xl font-black text-center text-gray-100 sm:text-4xl md:text-5xl lg:text-6xl"
+    class="max-w-5xl text-3xl font-black text-center text-gray-100 sm:text-4xl md:text-5xl lg:text-6xl !leading-[1.2]"
   >
-    Modern-looking, Free and Open-Source CAD/MDT system for FiveM.
+    Modern-looking, free and open-source CAD/MDT for FiveM.
   </h1>
 
-  <p class="max-w-3xl text-base text-center mt-7 md:text-xl">
+  <p class="max-w-3xl text-base text-center mt-2 md:text-xl">
     SnailyCAD is a free and open-source CAD/MDT made to feel and look modern with all the wanted
     features to fit your roleplay needs.
   </p>
 
-  <div class="w-full max-w-[700px]">
+  <div class="w-full max-w-[700px] mt-9">
     <LiteYouTube
       videoId="z30EmXXmJ_Q"
       playLabel="SnailyCADv4 Quick Walk-through - 2022"
--- svelte.config.js
+++ svelte.config.js
@@ -7,6 +7,7 @@ const config = {
 
   kit: {
     adapter: adapter(),
+    prerender: { default: true },
   },
 };
 
