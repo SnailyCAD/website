15ed314f0831dbcbd8968d6d32394401b5181d92
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-09-16 16:55:01 +0200

    :hammer: use cloudflare adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-cloudflare";
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: preprocess(),
   kit: {
-    adapter: adapter({ fallback: "200.html" }),
+    adapter: adapter(),
   },
 };
 
