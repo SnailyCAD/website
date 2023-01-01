8e5fd18254a56aea569db7841fb2593b89c9513d
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2023-01-01 09:48:48 +0100

    chore: upgrade deps

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,9 +8,6 @@
 </script>
 
 <svelte:head>
-  <meta charSet="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <meta name="robots" content="index, follow" />
   <title>SnailyCAD - The Free and open-source CAD/MDT for FiveM</title>
 
   <link rel="canonical" href="https://snailycad.caspertheghost.me" />
